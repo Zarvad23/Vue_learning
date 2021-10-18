1793d615fa553ef8c627dd7206c7d791c3d34bdf
Zarvad23 <zarifullinvadim@mail.ru>
2021-10-18 22:53:46 +0500

    Урок 1

---
+++ assets/js/main.js
@@ -0,0 +1,15 @@
+let app = new Vue({
+	el: '#app',
+	data: {
+		courseName: 'Какое либо название',
+		userName: 'Дорогой друг!',
+		tasks: ['Задача 1', 'Задача 2', 'Человек паук'],
+		forms: {
+			input1: null,
+			textarea: null,
+			checkboxes: [],
+			select: null
+		}
+	}
+})
+
